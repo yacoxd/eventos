df074339d3151742a5f70df0d9cdecccfecde0a0
Abner Tellez <atellezsazo@gmail.com>
2016-02-07 17:12:33 -0600

    First commit

---
+++ www/js/user.js
@@ -0,0 +1,31 @@
+angular.module('events.user', [])
+	.factory('User', function($http) {
+
+		var apiUrl = 'http://localhost/eventos/api/users/user';
+  
+  		var loggedIn = false;
+
+		return {
+
+			login: function(credentials,callback) {
+                
+                var auth = btoa(credentials.user + ":" + credentials.password);
+                var authorization = {'Authorization': 'Basic ' + auth};
+                var header = { headers: authorization }
+                
+				return $http.get(apiUrl, header)
+                .success(function(data){
+				    $http.defaults.headers.common.Authorization = 'Basic ' + auth;
+					loggedIn = true;
+                    callback(data);
+                }).error(function(){
+                    callback(false);
+                });
+			},
+
+			isLoggedIn: function() {
+				return loggedIn;
+			}
+
+		};
+	});
