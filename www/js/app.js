ce74dcc420454fee244223c787729df282644f54
Abner Tellez <atellezsazo@gmail.com>
2016-02-21 18:33:34 -0600

    Registro

--- www/js/app.js
+++ www/js/app.js
@@ -147,6 +147,133 @@ app.controller('ProfileCtrl', function($scope, $cookieStore, $rootScope, $ionicP
     
 });
 
+app.controller('RegisterCtrl', function($scope, $cookieStore, $rootScope, $ionicPopup, $filter, $state, $http, $ionicHistory, User) {
+    
+     var username = "";
+     
+     $scope.user_info = [];
+     
+     $scope.sexList = [
+        { text: "Masculino", value: "M" },
+        { text: "Femenino", value: "F" },
+    ];
+
+    $scope.data = {
+          sex: "",
+          first_name: "",
+          email: "",
+          last_name:"",
+          username: "",
+          pwd: "",
+          pwd2: "",
+          code: "",
+    };
+                  
+     $scope.save = function() {
+         
+         if ($scope.data.sex == "" || $scope.data.first_name == "" || $scope.data.last_name == ""
+            || $scope.data.username == "" || $scope.data.pwd == "" || $scope.data.pwd2 == "" || $scope.data.email == ""
+            || $scope.data.code == "") {
+             
+             var alertPopupMsg = $ionicPopup.alert({
+                                        title: 'Información',
+                                        template: "Todos los campos son requeridos."
+                                        });
+             alertPopupMsg.then();
+             
+         } else if (!validateEmail($scope.data.email)){
+             
+             var alertPopupMsg = $ionicPopup.alert({
+                                        title: 'Información',
+                                        template: "Email invalido."
+                                        });
+             alertPopupMsg.then();
+             
+         } else if (!validateUsername($scope.data.username)) {
+             
+             var alertPopupMsg = $ionicPopup.alert({
+                                        title: 'Información',
+                                        template: "Username invalido."
+                                        });
+             alertPopupMsg.then();
+             
+         } else if (! angular.equals(new String($scope.data.pwd), new String($scope.data.pwd2))) {
+             
+             var alertPopupMsg = $ionicPopup.alert({
+                                        title: 'Información',
+                                        template: "Las contraseñas no coinciden."
+                                        });
+             alertPopupMsg.then();
+             
+         } else {
+             
+            var apiPost = 'http://yacoxd.com/eventos/api/users/register';
+            
+            var authReg = btoa("register:guatemala");
+            var authorizationReg = {'Authorization': 'Basic ' + authReg};
+            var headerReg = { headers: authorizationReg }
+            
+            $http.post(apiPost, {email: $scope.data.email,
+                                username: $scope.data.username,
+                                first_name: $scope.data.first_name,
+                                last_name: $scope.data.last_name,
+                                pwd: $scope.data.pwd,
+                                gender: $scope.data.sex,
+                                code: $scope.data.code,
+                                }, headerReg).success(function(response, code){
+                                    
+                                    if (code == 200) {
+                                            var auth = btoa($scope.data.username + ":" + $scope.data.pwd);
+                                                
+                                            $rootScope.globals = {
+                                                    currentUser: {
+                                                        username: $scope.data.username,
+                                                        name: $scope.data.first_name + " " +$scope.data.last_name,
+                                                        image: "",
+                                                        authdata: auth,
+                                                    }
+                                                };
+                                                
+                                            $http.defaults.headers.common.Authorization = 'Basic ' + auth;
+                                            User.isLoggedSet(true);
+                                            
+                                            var alertPopupMsg = $ionicPopup.alert({
+                                                    title: 'Información',
+                                                    template: 'Se envio la informción correctamente.'
+                                                    });
+                                            alertPopupMsg.then();
+                                            
+                                            $ionicHistory.nextViewOptions({historyRoot: true});    
+                                            $state.go('menu.list');
+                                            
+                                    } else {
+                                        
+                                        console.log(response);
+                                        var alertPopupMsg2 = $ionicPopup.alert({
+                                                    title: 'Información',
+                                                    template: response.msg
+                                                    });
+                                            alertPopupMsg2.then();
+                                    }
+                                
+                                    
+            }).error(function(response){
+                
+                var alertPopupMsg = $ionicPopup.alert({
+                                        title: 'Información',
+                                        template: response.msg
+                                        });
+                alertPopupMsg.then();
+                
+            });
+         }
+         
+     }
+    
+    
+});
+
+
 app.controller('LoginCtrl', function($scope, $state, $ionicHistory, $ionicPopup,$rootScope, $cookieStore,  User) {
     
   $scope.credentials = {
@@ -292,6 +419,16 @@ app.controller('ListCtrl', function($scope, $cookieStore, $rootScope, $state, Us
     
 });
 
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateUsername(username) {
+    var re = /^[a-zA-Z0-9]+$/;
+    return re.test(username);
+}
+
 app.directive('errSrc', function() {
   return {
     link: function(scope, element, attrs) {
@@ -321,11 +458,11 @@ app.run(function($rootScope, $state, $ionicPlatform, $cookieStore, $http, User) 
   }
     
   $rootScope.$on('$stateChangeStart', function(event, toState) {
-    if (!User.isLoggedIn() && toState.name !== 'login') {
+    if (!User.isLoggedIn() && toState.name !== 'login' && toState.name !== 'register') {
       event.preventDefault();
       $state.go('login');
     } 
-  
+    
   });
 
   $ionicPlatform.ready(function() {
